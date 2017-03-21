761ca9745c4263dd0284bb5d84f2d54da0b79cc4
Alimşah Yıldırım <pumpings@hotmail.com>
2017-03-21 12:18:47 +0200

    Process Access Types

---
+++ ProcessAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IKVDirectXHook
+{
+    internal enum ProcessAccess : Int32
+    {
+        Terminate = 0x1,
+        CreateThread = 0x2,
+        VMOperation = 0x8,
+        VMRead = 0x10,
+        VMWrite = 0x20,
+        DuplicateHandle = 0x40,
+        SetInformation = 0x200,
+        QueryInformation = 0x400,
+        Synchronize = 0x100000
+    }
+}
