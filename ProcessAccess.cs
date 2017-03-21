6fa600ce1e9b8c9cd1deda728843a0d25f2cb33a
Alimşah Yıldırım <pumpings@hotmail.com>
2017-03-21 12:20:53 +0200

    Process access typtes

--- ProcessAccess.cs
+++ ProcessAccess.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+ *
+ * Copyright (C) 2017 Alimşah YILDIRIM <alimsahy@gmail.com>
+ *
+ * This file part of IKVDirectXHook
+ *
+ * IKVDirectXHook is free software: you can redistribute it and/or modify
+ * it under free terms of the GNU General Public Licence as published by
+ * the Free Software Foundation, either version 3 of the licence, or
+ * any later version
+ *
+ * IKVDirectXHook is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
