f7c1295863e0348f2b90f71b81b2490e6352ed4f
Alimşah Yıldırım <pumpings@hotmail.com>
2017-03-21 12:18:58 +0200

    Memory operations

---
+++ Memory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace IKVDirectXHook
+{
+    class Memory
+    {
+        private int processId;
+
+        private int processHandle;
+
+        public Memory(int process)
+        {
+            processId = process;
+            Process.EnterDebugMode();
+
+            processHandle = Kernel32.OpenProcess(ProcessAccess.VMRead | ProcessAccess.QueryInformation, false, processId);
+            if (processHandle == 0)
+            {
+                throw new Win32Exception("Unable to open process ID: " + processId);
+            }
+        }
+
+        ~Memory()
+        {
+            Kernel32.CloseHandle(processHandle);
+        }
+    }
+}
