83c982e9fb5155e0b27c0611a7b9ffa67b458df2
Alimşah Yıldırım <pumpings@hotmail.com>
2017-03-21 12:18:28 +0200

    Win32 Api
    Win32 Api

---
+++ Kernel32.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace IKVDirectXHook
+{
+    static class Kernel32
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Int32 OpenProcess(ProcessAccess dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] Boolean bInheritHandle, Int32 dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Boolean CloseHandle(Int32 hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Boolean ReadProcessMemory(Int32 hProcess, UInt32 lpBaseAddress, out Byte buffer, Int32 dwSize, out Int32 lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Boolean ReadProcessMemory(Int32 hProcess, UInt32 lpBaseAddress, out Int32 buffer, Int32 dwSize, out Int32 lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Boolean ReadProcessMemory(Int32 hProcess, UInt32 lpBaseAddress, out float buffer, Int32 dwSize, out Int32 lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Int32 GetCurrentProcess();
+    }
+}
